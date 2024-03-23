d3c3f137ec8859f4b37ac53a770066f1df116395
farrush1 <mfarras21@gmail.com>
2024-03-23 14:19:39 +0700

    homework 11 api todo

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "localhost",
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_TEST,
+    host: "localhost",
+    dialect: "postgres",
+  },
+  docker: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+   
+  },
+}
+
+module.exports = config
